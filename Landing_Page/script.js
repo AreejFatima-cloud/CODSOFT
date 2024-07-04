56548fe6014e563d43a083cbff4af6d5583ee1da
AreejFatima-cloud <fareej775@gmail.com>
2024-07-04 15:31:31 +0500

    Update script.js

--- Landing_Page/script.js
+++ Landing_Page/script.js
@@ -1,1 +1,12 @@
-
+ function toggleReadMore() {
+            var moreText = document.querySelector('.more-text');
+            var readMoreBtn = document.querySelector('.read-more');
+            
+            if (moreText.style.display === "none") {
+                moreText.style.display = "inline";
+                readMoreBtn.textContent = "Read Less";
+            } else {
+                moreText.style.display = "none";
+                readMoreBtn.textContent = "Read More";
+            }
+        }
